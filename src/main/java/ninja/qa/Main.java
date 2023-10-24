95bc6af6579c3ac112d597a0644aff750889d107
Dmitry Nazarov <dnazarov71317@gmail.com>
2023-10-24 19:56:37 +0400

    second commit eng lang

--- src/main/java/ninja/qa/Main.java
+++ src/main/java/ninja/qa/Main.java
@@ -5,7 +5,7 @@ package ninja.qa;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Привет друзья!");
+        System.out.println("hello world!");
 
     }
 }
